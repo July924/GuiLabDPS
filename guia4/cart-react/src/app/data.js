973f775ed97711887061c4207eb96feb2ff32055
July924 <marielaventura924@gmail.com>
2025-02-17 17:02:06 -0600

    resolucion de la guia4

---
+++ guia4/cart-react/src/app/data.js
@@ -0,0 +1,36 @@
+export const data=[
+    {
+    id:1,
+    title:'Cien años de soledad',
+    price:100,
+    urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity:1
+    },
+    {
+    id:2,
+    title:'El señor de los anillos(Trilogía)',
+    price:190,
+    urlImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThwzN4NDwaIaYjQiHgtYqKK0E_KKDgzec7BQ&s',
+    quantity:1
+    },
+    {
+    id:3,
+    title:'Cuentos de Barro',
+    price:30,
+    urlImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDYDau-y8-O2kQr-NPLUH_7EnCJ4mkYkPuGA&s',
+    quantity:1
+    },
+    {
+    id:4,
+    title:'Tierra de Infancia',
+    price:30,
+    urlImage:' https://assets.isu.pub/document-structure/230605011415-591c5b47dde190303e7de29a0a888041/v1/fa8a209bb2ce609861c6572f117e69f8.jpeg?width=720&quality=85%2C50',
+    quantity:1
+    }, {
+    id:5,
+    title:'Harry Potter Pack',
+    price:390,
+    urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+    quantity:1
+    }
+];
